3dd7f6976c3c18b72507636897e60fbf8d59fcaf
Sam Warner <sam-w@users.noreply.github.com>
2023-11-16 13:02:06 +1100

    detect rate limit from gtm api, back off and retry

--- examples/provider/provider.tf
+++ examples/provider/provider.tf
@@ -3,5 +3,5 @@ provider "gtm" {
   account_id                 = "6105084028"
   container_id               = "119458552"
   workspace_name             = "my-workspace"
-  max_api_queries_per_minute = 15
+  retry_limit                = 10
 }
